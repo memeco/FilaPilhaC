bb1f34f382d99087e3a5dcdeb277035e68bf479c
Lucas Quadros <olucasquadros@gmail.com>
2024-04-10 21:08:08 -0300

    estrutura de dados

--- main.c
+++ main.c
@@ -3,14 +3,27 @@
 #include <string.h>
 
 // Definir o tamanho
+#define MAX_SIZE 100
 
 // Estrutura para representar um livro
+typedef struct {
+    char title[100];
+} Book;
 
 // Estrutura para representar uma fila de livros
+typedef struct {
+    Book items[MAX_SIZE];
+    int front, rear;
+} Queue;
 
 // Estrutura para representar uma pilha de livros
+typedef struct {
+    Book items[MAX_SIZE];
+    int top;
+};
 
 // Função para inicializar uma filas
+
 
 // Função para verificar se a fila está vazia
 
