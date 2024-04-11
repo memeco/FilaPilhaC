f15ea27ba1a20d3a4e4c418848176d32c5b24e46
Lucas Quadros <olucasquadros@gmail.com>
2024-04-10 21:00:36 -0300

    algoritmo

---
+++ main.cc
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Definir o tamanho
+
+// Estrutura para representar um livro
+
+// Estrutura para representar uma fila de livros
+
+// Estrutura para representar uma pilha de livros
+
+// Função para inicializar uma filas
+
+// Função para verificar se a fila está vazia
+
+// Função para enfileirar um livro
+
+// Função para desenfileirar um livro
+
+// Função para inicializar uma pilha
+
+// Função para verificar se a pilha está vazia
+
+// Função para empilhar um livro
+
+// Função para desempilhar um livro
+
+
+int main() {
+
+}
